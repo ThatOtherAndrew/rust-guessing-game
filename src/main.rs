c330d6616ba31f351733a8ca6057813254251e22
Andrew <stroev.andrew@gmail.com>
2021-12-20 13:33:07 +0000

    ✨ Create functional guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::cmp::Ordering;
+use std::io;
+use rand::Rng;
+
+
 fn main() {
-    println!("Hello, world!");
+    let answer = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("Your guess:");
+
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line!");
+        let guess: u32 = guess.trim().parse().expect("Invalid input number!");
+
+        println!("You guessed {}...", guess);
+
+        match guess.cmp(&answer) {
+            Ordering::Less => println!("but it was too small!"),
+            Ordering::Greater => println!("but it was too big!"),
+            Ordering::Equal => {
+                println!("and you were correct!");
+                break;
+            }
+        }
+    }
 }
